a3e8661498651392702c132d98f70d4116b634e3
Ben Stephens <ben_j_stephens@hotmail.com>
2020-09-07 14:30:24 +1000

    neated up code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,33 +5,30 @@ module.exports = function (app) {
 
     app.get("/api/workouts", (req, res) => {
         Workout.find()
-        .then(data => {
-            res.json(data)
-        })
-        .catch(err => {
-            console.error("error message", err);
-        });
+        .then(data => res.json(data))
+        .catch(err => console.error("error message", err));
     })
+
+    app.get("/api/workouts/range", (req, res) => {
+        Workout.find()
+        .then(data => res.json(data))
+        .catch(err => console.error("error message", err));
+    })
+
 
     app.post("/api/workouts", (req, res) => {
         Workout.create({})
-        .then(data => {
-            res.json(data)
-        }).catch(err => {
-            console.error("error message", err);
-        }) 
+        .then(data => res.json(data))
+        .catch(err => console.error("error message", err));
     })
 
     app.put("/api/workouts/:id", (req, res) => {
         Workout.findByIdAndUpdate(
             req.params.id, 
-            { $push: { exercise: req.body} }, 
+            { $push: { exercises: req.body} }, 
             { new: true, runValidators: true }
         )
         .then(data => res.json(data))
-        .catch(err => 
-            {
-                console.error("error message", err)
-            })
+        .catch(err => console.error("error message", err))
     });
 }
