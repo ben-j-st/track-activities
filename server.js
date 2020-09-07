fcc21e5aaa132a7e18a96767fb363cfbd1b1c9f8
Ben Stephens <ben_j_stephens@hotmail.com>
2020-09-07 12:52:31 +1000

    changed server routes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 // Setting up Express App
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.static("public"));
@@ -12,12 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // routes
-app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/htmlRoutes.js"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 // db mongo
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
