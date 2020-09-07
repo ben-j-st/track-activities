83e4331cc53746917c6210e438371e7273ed35cd
Ben Stephens <ben_j_stephens@hotmail.com>
2020-09-07 13:18:52 +1000

    exported routes porperly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,47 +3,44 @@ const Workout = require("../models/workout.js");
 // Get users last workout
 module.exports = function (app) {
 
-app.get("/api/workouts", (req, res) => {
-    Workout.find({}, (err, data) => {
-        if (err) {
-            console.log(err)
-        }
-        res.json(data)
-    })
-})
-
-app.post("/api/workouts", (req, res) => {
-    console.log("trying to add a workout \n");
-    Workout.create({})
-    .then(workoutData => {
-        console.error(workoutData);
-        res.json(workoutData)
-    }).catch(err => {
-        console.log("err", err);
-    }) 
-})
-
-app.put("/api/workouts/:id", (req, res) => {
-    console.log("running an update on a workout \n")
-    const id = req.params.id;
-
-    Workout.update(
-        {
-            id
-        }, 
-        { 
-            $push: {
-                exercise: req.body
-            }    
-        }, 
-        {
-            new: true
-        }, (err, data) => {
+    app.get("/api/workouts", (req, res) => {
+        Workout.find({}, (err, data) => {
             if (err) {
-                console.log(err);
+                console.log(err)
             }
             res.json(data)
-        }
-    )
-})
+        })
+    })
+
+    app.post("/api/workouts", (req, res) => {
+        Workout.create({})
+        .then(data => {
+            res.json(data)
+        }).catch(err => {
+            console.log("err", err);
+        }) 
+    })
+
+    app.put("/api/workouts/:id", (req, res) => {
+        const id = req.params.id;
+
+        Workout.update(
+            {
+                id
+            }, 
+            { 
+                $push: {
+                    exercise: req.body
+                }    
+            }, 
+            {
+                new: true
+            }, (err, data) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.json(data)
+            }
+        )
+    })
 }
